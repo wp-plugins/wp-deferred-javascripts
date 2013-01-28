e3d1352b0a3ac0f05dde549f1a87fee6de0204eb
willybahuaud <willybahuaud@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-28 22:06:47 +0000

    anglicisation du plugin :-P
    git-svn-id: https://plugins.svn.wordpress.org/wp-deferred-javascripts/trunk@660351 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-deferred-javascripts.php
+++ wp-deferred-javascripts.php
@@ -3,106 +3,97 @@
 Plugin Name: WP deferred javaScript
 Plugin URI: http://www.wabeo.fr
 Description: This plugin defer the loading of all javascripts added by the way of wp_enqueue_scripts, using LABJS.
-Version:1.0
-Author: Willy Bahuaud, confridin
+Version:1.1
+Author: Willy Bahuaud, Daniel Roch
 Author URI: http://wabeo.fr
 */
 /**
-INITIALIZATION VARIABLE GLOBALE
-* @var delayed_script ARRAY 
+INITIALIZATION OF OUR GLOBAL VARIABLE
+* @var all_our_scripts ARRAY 
 */
-add_action('init','initialize_script_global');
-function initialize_script_global(){
-	global $delayed_script;
-	$delayed_script = array();
+function initialize_script_global() {
+	global $all_our_scripts;
+	$all_our_scripts = array();
 }
-
+add_action( 'init', 'initialize_script_global' );
 /**
-POUSSAGE DES SCRIPTS DANS LA GLOBALE & VIDAGE DE $WP_SCRIPTS
+PUSH SCRIPTS INTO OUR GLOBAL & EMPTYING $WP_SCRIPTS
+* @uses FILTER all_our_scripts_datas
 */
-add_action('wp_head','stop',1);
-add_action('wp_footer','stop',1);
-function stop(){
-	global $delayed_script,$wp_scripts;
-	foreach($wp_scripts->queue as $s){
-		$delayed_script[$wp_scripts->registered[$s]->handle] = array(
+function you_shall_not_pass() {
+	global $all_our_scripts, $wp_scripts;
+	foreach( $wp_scripts->queue as $s ) {
+		$all_our_scripts[$wp_scripts->registered[$s]->handle] = array(
 			'src'   => $wp_scripts->registered[$s]->src, 
 			'deps'  => $wp_scripts->registered[$s]->deps, 
 			'extra' =>$wp_scripts->registered[$s]->extra
 			);
-		apply_filters('delayed_script_datas', $delayed_script);
+		apply_filters( 'all_our_scripts_datas', $all_our_scripts );
 	}
 	$wp_scripts->queue = array();
 }
+add_action( 'wp_head', 'you_shall_not_pass', 9 );
+add_action( 'wp_footer', 'you_shall_not_pass', 9 );
 
 /**
-RENDU FINAL DES SCRIPTS
+FINAL REPORT OF SCRIPTS
 */
-add_action('wp_footer','afficher_moi_tout_ca',99);
-function afficher_moi_tout_ca(){
-	global $delayed_script,$wp_scripts;
+function cross_the_steams() {
+	global $all_our_scripts, $wp_scripts;
 
-	//WHILE
-	//i = nombr de script
-	//a chaque fois qu'un script est ajoute, on desiterre i
-	//les scripts sans dependance sont jetés
-	//les scripts avec depandances sont remis au lendemain
-	//lorsque i = 0 -> on sort
-	$i = count($delayed_script);
+	$i = count( $all_our_scripts );
 
-	$starscriptethutch = array();
-	$mortvivant = $delayed_script;
+	$all_our_ordered_scripts = array();
+	$undead = $all_our_scripts;
 
-	while($i>0){
-		foreach($delayed_script as $k => $s){
-			if(empty($s['deps'])){
-				$starscriptethutch[$k] = $s;
-				unset($delayed_script[$k]);
+	while( $i > 0 ) {
+		foreach( $all_our_scripts as $k => $s ) {
+			if( empty( $s['deps'] ) ){
+				$all_our_ordered_scripts[$k] = $s;
+				unset( $all_our_scripts[$k] );
 				$i--;
 			}else{
-				$alldepscounter = 0; //nombre de conditions remplies
-				foreach($s['deps'] as $d){
-					if(!array_key_exists($d,$mortvivant)){ //je n'aurais jamais l'indépendance...
-						if(isset($wp_scripts->registered[$d])){ //... à moins que quelqu'un puisse me sauver
-							$delayed_script[$wp_scripts->registered[$d]->handle] = array(
+				$alldepscounter = 0; //number of satisfied conditions
+				foreach( $s['deps'] as $d ){
+					if( !array_key_exists( $d, $undead ) ) { //can I load me ?
+						if( isset( $wp_scripts->registered[$d] ) ) { // yes you can
+							$all_our_scripts[$wp_scripts->registered[$d]->handle] = array(
 								'src'   => $wp_scripts->registered[$d]->src,
 								'deps'  => $wp_scripts->registered[$d]->deps, 
 								'extra' =>$wp_scripts->registered[$d]->extra);
-							$mortvivant[$wp_scripts->registered[$d]->handle] = $delayed_script[$wp_scripts->registered[$d]->handle];
+							$undead[$wp_scripts->registered[$d]->handle] = $all_our_scripts[$wp_scripts->registered[$d]->handle];
 						}else{
-							unset($delayed_script[$k]);
+							unset( $all_our_scripts[$k] );
 							$i--;
-							break; //retourne dans ton monde, créature démoniaque...
+							break; //go back into darkness demonic creature ...
 						}
 					}else{
-						if(array_key_exists($d,$starscriptethutch)){ // je rempli une condition en +
+						if( array_key_exists( $d, $all_our_ordered_scripts ) ) // one more satisfied condition
 							$alldepscounter++;
-						}
 					}
 				}
-				if($alldepscounter == count($s['deps'])){ //bravo, toutes les conditions sont remplies
-					$starscriptethutch[$k] = $s;
-					unset($delayed_script[$k]);
+				if( $alldepscounter == count( $s['deps'] ) ) { //YEAH, all conditions are satisfied now !!
+					$all_our_ordered_scripts[$k] = $s;
+					unset( $all_our_scripts[$k] );
 					$i--;
 				}
 			}
 		}
 	}
-	$delayed_script = $starscriptethutch; //le même, mais dans l'ordre ^^
-
-	rendez_moi_mes_scripts($delayed_script); //on écrit les scripts
+	render_our_scripts_now( $all_our_ordered_scripts ); //print scripts
 }
+add_action( 'wp_footer', 'cross_the_steams', 99 );
 
 /**
-FONCTION UTILISÉE POUR LE RENDU
+FUNCTION USED FOR RENDERING SCRIPTS
 */
-function rendez_moi_mes_scripts($delayed_script){
-	if(!empty($delayed_script)){
-		$output = '<script src="'.plugin_dir_url(__FILE__).'j/lab.min.js"></script>'."\n";
-		$output.= '<script>';
-		foreach($delayed_script as $s)
-			$output.= $s['extra']['data'].'$LAB.script("'.$s['src'].'");'."\n";
-		$output.= '</script>';
+function render_our_scripts_now( $all_our_scripts ) {
+	if( !empty( $all_our_scripts ) ) {
+		$output  = '<script src="'.plugin_dir_url( __FILE__ ).'j/lab.min.js"></script>'."\n";
+		$output .= '<script>';
+		foreach($all_our_scripts as $s)
+			$output .= $s['extra']['data'].'$LAB.script("'.$s['src'].'");'."\n";
+		$output .= '</script>';
 		echo $output;
 	}
 }
